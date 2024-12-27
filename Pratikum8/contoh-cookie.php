0dcfcb9a50dc16eaaec523a44195f3449e00fec6
dewirahayu0512 <dewirahayu2778@gmail.com>
2024-12-27 17:34:54 +0700

    TUGAS 8

---
+++ Pratikum8/contoh-cookie.php
@@ -0,0 +1,58 @@
+<<<<<<< HEAD
+<?php 
+    $value = "Ini Cookie Pertama";
+    if(!isset($_COOKIE['first_cookie'])){
+        setcookie("first_cookie", $value, time() + 3600 * 24 * 30);
+    }
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Implementasi Cookie</h1>
+    <?php if(isset($_COOKIE['first_cookie'])):?>
+        <p><?php echo $_COOKIE['first_cookie'];?></p>
+    <?php else:?>
+        <p>Cookie belum di-set</p>
+    <?php endif;?>
+
+    <!--menghapus cookie -->
+    <?php
+        if(isset($_COOKIE['first_cookie'])){
+            setcookie('first_cookie');
+        }
+    ?>
+</body>
+=======
+<?php 
+    $value = "Ini Cookie Pertama";
+    if(!isset($_COOKIE['first_cookie'])){
+        setcookie("first_cookie", $value, time() + 3600 * 24 * 30);
+    }
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Implementasi Cookie</h1>
+    <?php if(isset($_COOKIE['first_cookie'])):?>
+        <p><?php echo $_COOKIE['first_cookie'];?></p>
+    <?php else:?>
+        <p>Cookie belum di-set</p>
+    <?php endif;?>
+
+    <!--menghapus cookie -->
+    <?php
+        if(isset($_COOKIE['first_cookie'])){
+            setcookie('first_cookie');
+        }
+    ?>
+</body>
+>>>>>>> 127fc40d74a3b4854e1fb4e7281700fd4d1273e7
+</html>
